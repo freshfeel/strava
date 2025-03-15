6df0070a87aeb104be55e723d683c88dd8e4d2be
Fresh Feel <damidoppler@gmail.com>
2025-03-15 19:47:49 +0100

    Add athlete profile information display

---
+++ oauth_token.sh
@@ -0,0 +1,5 @@
+curl -X POST https://www.strava.com/oauth/token \
+	-F client_id=151040 \
+	-F client_secret=b07884dfae713d3235489a84eb6ca7261ceab4b1 \
+	-F code=4f014288b73161a6e60a668c78d70928ab6e6618 \
+	-F grant_type=authorization_code
